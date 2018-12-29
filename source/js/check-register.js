e15adbe381b9f2ddb6ccbedeed8b340ef80c955b
jorgeglezca98 <jorgeglezca98@gmail.com>
2018-12-29 20:34:10 +0000

    Ya deja registrar a falta de un par de detalles.

--- source/js/check-register.js
+++ source/js/check-register.js
@@ -1,12 +1,49 @@
+function addUser() {
+    var email = document.getElementById('email-field').value;
+    var password = document.getElementById('password-field').value;
+    var age = parseInt(document.getElementById('age-field').value);
+    var address = document.getElementById('address-field').value;
+    var phone = parseInt(document.getElementById('phone-field').value);
+    var gender = "";
+    if (document.getElementById('customRadio').checked) {
+        gender = "Male";
+    } else {
+        gender = "Female";
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+        firebase.database().ref("users").push().set({
+            email: email,
+            age: age,
+            address: address,
+            phone: phone,
+            gender: gender
+        });
+        document.getElementById('register-form').submit();
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+}
+
 function checkRegisterForm() {
-    debugger;
     if (!document.getElementById('email-field').value.match(/^.{2,}@.{2,}\..{2,}$/))
         return false;
-    if (document.getElementById('username-field').value.length == 0)
+    if (document.getElementById('password-field').value.length < 5)
         return false;
     if (document.getElementById('phone-field').value.length != 9)
         return false;
-    if (document.getElementById('age-field').value.length < 18)
+    if (parseInt(document.getElementById('age-field').value) < 18)
         return false;
-    return true;
+        
+    addUser();
 }
