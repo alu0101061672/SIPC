dd0dea027600cb0060cb636d87e8f89410692580
jorgeglezca98 <jorgeglezca98@gmail.com>
2019-01-02 17:54:04 +0000

    Ahora se tienen que completar los campos para añadir al carrito.

--- source/js/initialize-details.js
+++ source/js/initialize-details.js
@@ -7,6 +7,11 @@ firebase.database().ref("products/" + productId).on("value", function (snap) {
     document.getElementById("price").innerHTML = changedPost.price + "€";
     document.getElementById("picture").setAttribute("src", changedPost.pictures);
     document.getElementById('add-selected-product').onclick = function () {
-        addToCart(parseInt(productId), parseInt(document.getElementById('quantity-input').value), "a", changedPost.name, changedPost.price);
+        if (document.getElementById("size-selection").selectedIndex != 0 && 
+            document.getElementById("color-selection").selectedIndex != 0) {
+            addToCart(parseInt(productId), parseInt(document.getElementById('quantity-input').value), "a", changedPost.name, changedPost.price);
+        } else {
+            alert('Complete all fields.');
+        }
     };
 });
